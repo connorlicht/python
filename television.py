e5e0fdd0675a62d5c9e0fbb70005ae0dc4bde8f3
Connor Licht <colicht2004@gmail.com>
2023-11-15 16:20:14 -0600

    Fixed some things

--- television.py
+++ television.py
@@ -7,23 +7,23 @@ class Television():
     MIN_CHANNEL: int = 0
     MAX_CHANNEL: int = 3
     
-    def __init__(self):
+    def __init__(self) -> None:
         """
         Method to initialize the attributes of the Television class.
         """
         self.__status: bool = False
         self.__muted: bool = False
-        self.__volume: int = self.MIN_VOLUME
-        self.__channel: int = self.MIN_CHANNEL
+        self.__volume: int = Television.MIN_VOLUME
+        self.__channel: int = Television.MIN_CHANNEL
         self.__stored_volume: int = 0
         
-    def power(self):
+    def power(self) -> None:
         """
         Method to toggle the power of the television.
         """
         self.__status = not self.__status
         
-    def mute(self):
+    def mute(self) -> None:
         """
         Method to mute and unmute the television.
         """
@@ -31,32 +31,32 @@ class Television():
             if self.__muted == False:
                 self.__muted = True
                 self.__stored_volume = self.__volume
-                self.__volume = self.MIN_VOLUME
+                self.__volume = Television.MIN_VOLUME
             else:
                 self.__muted = False
                 self.__volume = self.__stored_volume
     
-    def channel_up(self):
+    def channel_up(self) -> None:
         """
         Method to increase the channel of the television.
         """
         if self.__status == True:
-            if self.__channel < self.MAX_CHANNEL:
+            if self.__channel < Television.MAX_CHANNEL:
                 self.__channel += 1
             else:
-                self.__channel = self.MIN_CHANNEL
+                self.__channel = Television.MIN_CHANNEL
                 
-    def channel_down(self):
+    def channel_down(self) -> None:
         """
         Method to decrease the channel of the television.
         """
         if self.__status == True:
-            if self.__channel > self.MIN_CHANNEL:
+            if self.__channel > Television.MIN_CHANNEL:
                 self.__channel -= 1
             else:
-                self.__channel = self.MAX_CHANNEL
+                self.__channel = Television.MAX_CHANNEL
                 
-    def volume_up(self):
+    def volume_up(self) -> None:
         """
         Method to increase the volume of the television.
         """
@@ -64,10 +64,10 @@ class Television():
             if self.__muted == True:
                 self.__muted = False
                 self.__volume = self.__stored_volume
-            if self.__volume < self.MAX_VOLUME:
+            if self.__volume < Television.MAX_VOLUME:
                 self.__volume += 1
 
-    def volume_down(self):
+    def volume_down(self) -> None:
         """
         Method to decrease the volume of the television.
         """
@@ -75,10 +75,10 @@ class Television():
             if self.__muted == True:
                 self.__muted = False
                 self.__volume = self.__stored_volume
-            if self.__volume > self.MIN_VOLUME:
+            if self.__volume > Television.MIN_VOLUME:
                 self.__volume -= 1
                 
-    def __str__(self):
+    def __str__(self) -> str:
         """
         Method to return the string representation of the Television class.
         :return: The string representation of the Television class.
