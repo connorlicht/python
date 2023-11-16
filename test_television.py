4e0c2fed36b6497b5d5b8b6c9cc16798fbfb179e
Connor Licht <colicht2004@gmail.com>
2023-11-15 19:44:02 -0600

    Fixed some things

--- test_television.py
+++ test_television.py
@@ -6,97 +6,96 @@ class Test:
     Class to test the Television class.
     """
     
+    def setup_method(self) -> None:
+        """
+        Method to setup the test fixture for the Television class.
+        """
+        self.tv = Television()
+    
     def test__init__(self) -> None:
         """
         Method to test the __init__ method of the Television class.
         """
-        tv = Television()
-        assert tv.__str__() == "Power = False, Channel = 0, Volume = 0" # Default values when initialized
+        assert self.tv.__str__() == "Power = False, Channel = 0, Volume = 0" # Default values when initialized
         
     def test_power(self) -> None:
         """
         Method to test the power method of the Television class.
         """
-        tv = Television()
-        tv.power()
-        assert tv.__str__() == "Power = True, Channel = 0, Volume = 0" # Powered on
-        tv.power()
-        assert tv.__str__() == "Power = False, Channel = 0, Volume = 0" # Powered off
+        self.tv.power()
+        assert self.tv.__str__() == "Power = True, Channel = 0, Volume = 0" # Powered on
+        self.tv.power()
+        assert self.tv.__str__() == "Power = False, Channel = 0, Volume = 0" # Powered off
 
     def test_mute(self) -> None:
         """
         Method to test the mute method of the Television class.
         """
-        tv = Television()
-        tv.power()
-        tv.volume_up()
-        tv.mute()
-        assert tv.__str__() == "Power = True, Channel = 0, Volume = 0" # Powered on volume turned up then muted
-        tv.mute()
-        assert tv.__str__() == "Power = True, Channel = 0, Volume = 1" # Powered on volume turned up then unmuted
-        tv.power()
-        assert tv.__str__() == "Power = False, Channel = 0, Volume = 1" # Powered off with volume already turned up then tries to mute with it off.
-        tv.power()
-        tv.mute()
-        tv.power()
-        assert tv.__str__() == "Power = False, Channel = 0, Volume = 0" # Turned back on muted then turned off with volume already turned up.
+        self.tv.power()
+        self.tv.volume_up()
+        self.tv.mute()
+        assert self.tv.__str__() == "Power = True, Channel = 0, Volume = 0" # Powered on volume turned up then muted
+        self.tv.mute()
+        assert self.tv.__str__() == "Power = True, Channel = 0, Volume = 1" # Powered on volume turned up then unmuted
+        self.tv.power()
+        assert self.tv.__str__() == "Power = False, Channel = 0, Volume = 1" # Powered off with volume already turned up then tries to mute with it off.
+        self.tv.power()
+        self.tv.mute()
+        self.tv.power()
+        assert self.tv.__str__() == "Power = False, Channel = 0, Volume = 0" # Turned back on muted then turned off with volume already turned up.
         
     def test_channel_up(self) -> None:
         """
         Method to test the channel_up method of the Television class.
         """
-        tv = Television()
-        tv.channel_up()
-        assert tv.__str__() == "Power = False, Channel = 0, Volume = 0" # Attempt to channel up with tv off
-        tv.power()
-        tv.channel_up()
-        assert tv.__str__() == "Power = True, Channel = 1, Volume = 0" # Channel up with tv on
-        tv.channel_up()
-        tv.channel_up()
-        tv.channel_up()
-        assert tv.__str__() == "Power = True, Channel = 0, Volume = 0" # Channel up with tv on and at max channel
+        self.tv.channel_up()
+        assert self.tv.__str__() == "Power = False, Channel = 0, Volume = 0" # Attempt to channel up with tv off
+        self.tv.power()
+        self.tv.channel_up()
+        assert self.tv.__str__() == "Power = True, Channel = 1, Volume = 0" # Channel up with tv on
+        self.tv.channel_up()
+        self.tv.channel_up()
+        self.tv.channel_up()
+        assert self.tv.__str__() == "Power = True, Channel = 0, Volume = 0" # Channel up with tv on and at max channel
         
     def test_channel_down(self) -> None:
         """
         Method to test the channel_down method of the Television class.
         """
-        tv = Television()
-        tv.channel_down()
-        assert tv.__str__() == "Power = False, Channel = 0, Volume = 0" # Attempt to channel down with tv off
-        tv.power()
-        tv.channel_down()
-        assert tv.__str__() == "Power = True, Channel = 3, Volume = 0" # Channel down with tv on and at minimum channel
+        self.tv.channel_down()
+        assert self.tv.__str__() == "Power = False, Channel = 0, Volume = 0" # Attempt to channel down with tv off
+        self.tv.power()
+        self.tv.channel_down()
+        assert self.tv.__str__() == "Power = True, Channel = 3, Volume = 0" # Channel down with tv on and at minimum channel
         
     def test_volume_up(self) -> None:
         """
         Method to test the volume_up method of the Television class.
         """
-        tv = Television()
-        tv.volume_up()
-        assert tv.__str__() == "Power = False, Channel = 0, Volume = 0" # Attempt to volume up with tv off
-        tv.power()
-        tv.volume_up()
-        assert tv.__str__() == "Power = True, Channel = 0, Volume = 1" # Volume up with tv on
-        tv.mute()
-        tv.volume_up()
-        assert tv.__str__() == "Power = True, Channel = 0, Volume = 2" # Volume up with tv on and muted
-        tv.volume_up()
-        assert tv.__str__() == "Power = True, Channel = 0, Volume = 2" # Volume up with tv on and at max volume
+        self.tv.volume_up()
+        assert self.tv.__str__() == "Power = False, Channel = 0, Volume = 0" # Attempt to volume up with tv off
+        self.tv.power()
+        self.tv.volume_up()
+        assert self.tv.__str__() == "Power = True, Channel = 0, Volume = 1" # Volume up with tv on
+        self.tv.mute()
+        self.tv.volume_up()
+        assert self.tv.__str__() == "Power = True, Channel = 0, Volume = 2" # Volume up with tv on and muted
+        self.tv.volume_up()
+        assert self.tv.__str__() == "Power = True, Channel = 0, Volume = 2" # Volume up with tv on and at max volume
         
     def test_volume_down(self) -> None:
         """
         Method to test the volume_down method of the Television class.
         """
-        tv = Television()
-        tv.volume_down()
-        assert tv.__str__() == "Power = False, Channel = 0, Volume = 0" # Attempt to volume down with tv off
-        tv.power()
-        tv.volume_up()
-        tv.volume_up()
-        tv.volume_down()
-        assert tv.__str__() == "Power = True, Channel = 0, Volume = 1" # Volume down with tv on
-        tv.volume_down()
-        tv.mute()
-        assert tv.__str__() == "Power = True, Channel = 0, Volume = 0" # Volume down with tv on and muted
-        tv.volume_down()
-        assert tv.__str__() == "Power = True, Channel = 0, Volume = 0" # Volume down with tv on and at min volume
+        self.tv.volume_down()
+        assert self.tv.__str__() == "Power = False, Channel = 0, Volume = 0" # Attempt to volume down with tv off
+        self.tv.power()
+        self.tv.volume_up()
+        self.tv.volume_up()
+        self.tv.volume_down()
+        assert self.tv.__str__() == "Power = True, Channel = 0, Volume = 1" # Volume down with tv on
+        self.tv.volume_down()
+        self.tv.mute()
+        assert self.tv.__str__() == "Power = True, Channel = 0, Volume = 0" # Volume down with tv on and muted
+        self.tv.volume_down()
+        assert self.tv.__str__() == "Power = True, Channel = 0, Volume = 0" # Volume down with tv on and at min volume
